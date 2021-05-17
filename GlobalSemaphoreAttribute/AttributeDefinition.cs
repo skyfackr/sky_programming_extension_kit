03be9f57fbc7deac6fa42e857504e8ec260149f9
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2021-05-17 17:56:17 +0800

    202105171756

--- GlobalSemaphoreAttribute/AttributeDefinition.cs
+++ GlobalSemaphoreAttribute/AttributeDefinition.cs
@@ -4,7 +4,12 @@ using System.Data.SqlTypes;
 namespace SPEkit.SemaphoreSlimAttribute
 {
     [AttributeUsage(AttributeTargets.Constructor|AttributeTargets.Delegate|AttributeTargets.Event|AttributeTargets.Field|AttributeTargets.Method|AttributeTargets.Module|AttributeTargets.Property,AllowMultiple = true,Inherited = false)]
-    public sealed partial class SemaphoreSlimAttribute : Attribute
+    public sealed partial class SlotWaitAttribute : Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Delegate | AttributeTargets.Event | AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Module | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+    public sealed partial class SlotWaitAsyncAttribute : Attribute
     {
 
     }
