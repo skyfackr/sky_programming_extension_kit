19f8795487d98d43f6796f486b82b26ccfcf42bc
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2021-05-17 17:26:45 +0800

    202105171726

---
+++ GlobalSemaphoreAttribute/AttributeDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace SPEkit.SemaphoreSlimAttribute
+{
+    [AttributeUsage(AttributeTargets.Constructor|AttributeTargets.Delegate|AttributeTargets.Event|AttributeTargets.Field|AttributeTargets.Method|AttributeTargets.Module|AttributeTargets.Property,AllowMultiple = true,Inherited = false)]
+    public sealed partial class SemaphoreSlimAttribute : Attribute
+    {
+
+    }
+}
--- GlobalSemaphoreAttribute/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GlobalSemaphoreAttribute
-{
-    public class Class1
-    {
-    }
-}
