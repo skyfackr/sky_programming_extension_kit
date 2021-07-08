2bbf860c26dfb6e59c7d69322dbdffa989a0d706
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2021-07-08 16:31:53 +0800

    202107081630

---
+++ CombinedSemaphore/CombinedSemaphore/Waiting/WaitingSessions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPEkit.CombinedSemaphore.Unit;
+
+namespace SPEkit.CombinedSemaphore.Utils
+{
+    internal class WaitingSessions
+    {
+        internal readonly bool IsEntered;
+        internal readonly SemaphoreUnit Unit;
+
+        internal WaitingSessions(SemaphoreUnit unit,bool isEntered = false)
+        {
+            IsEntered = isEntered;
+            Unit = unit;
+        }
+    }
+}
